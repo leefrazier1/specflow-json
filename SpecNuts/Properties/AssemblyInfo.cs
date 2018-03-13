be59122a16b4c7ecbed45621f904c1e5852e37a8
Cosmin Stirbu <cosmin.stirbu@fortech.ro>
2018-03-13 15:53:43 +0200

    Fixes issue with async / await steps not being reported correctly

--- SpecNuts/Properties/AssemblyInfo.cs
+++ SpecNuts/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("301e04c6-bc1b-4da4-83f0-5fdf76fea8bb")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
