93c13066711e227ed350757d746eecb3bf580a81
Cosmin Stirbu <cosmin.stirbu@fortech.ro>
2018-03-13 17:15:58 +0200

    Expose embedding functionality on the Step class to allow embedding of screenshots for failed steps

--- SpecNuts/Model/Feature.cs
+++ SpecNuts/Model/Feature.cs
@@ -6,16 +6,16 @@ namespace SpecNuts.Model
 	{
 		private string _description;
 
-	    private static readonly string DefaultUri = "/uri/placeholder";
+		private static readonly string DefaultUri = "/uri/placeholder";
 
 		public string Description
 		{
 			get { return _description; }
 			set { _description = string.IsNullOrEmpty(value) ? value : value.Replace("\r", ""); }
 		}
-        public string Uri => DefaultUri;
-	    
-        public List<Scenario> Elements { get; set; }
-	    public new string Keyword => "Feature";
+		public string Uri { get; set; } = DefaultUri;
+
+		public List<Scenario> Elements { get; set; }
+		public new string Keyword => "Feature";
 	}
 }
